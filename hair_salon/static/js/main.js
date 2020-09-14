36a8c8d280e8bdae57833df94608f0b6b8f10cb8
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-09-14 20:32:03 +0100

    Added img grid

--- hair_salon/static/js/main.js
+++ hair_salon/static/js/main.js
@@ -1,3 +1,29 @@
+const current = document.getElementById('current')
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.5;
+
+// Setting opacity to first img
+imgs[0].style.opacity = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick));
+
+function imgClick(e) {
+    //Reset img opacity
+    imgs.forEach(img => (img.style.opacity = 1))
+    //change current img 
+    current.src = e.target.src
+    // Add fade-in class
+    current.classList.add('fade-in');
+    //Remove fade-in class setting timeout
+    setTimeout(()=> current.classList.remove('fade-in'),500);
+    //Change img opacticy setting to the variable opacity
+    e.target.style.opacity = opacity;
+}
+
+
+    
+
+
 //calculate the nav heigh
 let navbarHeight = $('.navbar').outerHeight();
 
