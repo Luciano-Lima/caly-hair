811fee86d8ae7ad29ec113c2a1022f668abce745
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-10-04 18:43:45 +0100

    Adding return self.name

--- hair_salon/models.py
+++ hair_salon/models.py
@@ -6,34 +6,52 @@ class Cutting(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
     class Meta:
-        verbose_name = 'cutting'
+        verbose_name = 'Cutting & Styling'
+
+    def __str__(self):
+        return self.name    
 
 class Styling(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
+    def __str__(self):
+        return self.name
 
 class Colour(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
-    
+    def __str__(self):
+        return self.name   
 
 class Highlights(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.name
 
 
 class Toners(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
+    def __str__(self):
+        return self.name
+
 
 class Treatment(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
 
+    def __str__(self):
+        return self.name
+
 
 class Extension(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return self.name
