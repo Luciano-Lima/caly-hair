9afb6ee1efe11aee67a37ca6fe2533d95c73a7f9
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-10-06 15:19:15 +0100

    adding media files location

--- AlinaApp/settings.py
+++ AlinaApp/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATICFILES_LOCATION = 'img'
-# MEDIAFILES_LOCATION = 'media'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_LOCATION = 'static'
+MEDIAFILES_LOCATION = 'img'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
