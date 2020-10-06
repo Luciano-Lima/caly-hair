7bb4b7f3d739ad34d08fadd2ad655b675ea46976
Luciano Lima <ccll4ever1@Lucianos-MacBook-Pro.local>
2020-10-06 16:09:17 +0100

    /

--- AlinaApp/settings.py
+++ AlinaApp/settings.py
@@ -126,6 +126,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "/static")]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'img')
 MEDIA_URL = '/img/'
